a7730d1f506ab75092bf832359be45f7a30841e7
Aleksandr Poliakov <Aleksandr.Poliakov@noveogroup.com>
2022-09-28 12:12:37 +0300

    Add constants, remove unneeded file APIService

---
+++ MoviesTest/AppConstants.swift
@@ -0,0 +1,14 @@
+//
+//  AppConstants.swift
+//  MoviesTest
+//
+//  Created by Aleksandr Poliakov on 28.09.2022.
+//
+
+import Foundation
+
+struct AppConstants {
+    static let apiKey = "c9856d0cb57c3f14bf75bdc6c063b8f3"
+    static let moviesListURL = "https://api.themoviedb.org/3/discover/movie"
+    static let movieDetailsURL = "https://api.themoviedb.org/3/movie/{movie_id}"
+}
